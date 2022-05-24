1213ff25f3ca8255eea697e8e8abc5b039930d26
jruano01 <101552889+jruano01@users.noreply.github.com>
2022-05-24 09:49:53 +0200

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -1,20 +1,17 @@
-import typer
-from ssg.site import Site
-
-import ssg.parsers
+from pathlib import Path
 
 
-def main(source="content", dest="dist"):
-    config = {
-        "source": source,
-        "dest": dest,
-        "parsers": [
-            ssg.parsers.ResourceParser(),
-            ssg.parsers.MarkdownParser(),
-            ssg.parsers.ReStructuredTextParser(),
-        ],
-    }
-    Site(**config).build()
+ class Site:
+     def __init__(self, source, dest):
+         self.source = Path(source)
+         self.dest = Path(dest)
 
+     def create_dir(self, path):
+         directory = self.dest / path.relative_to(self.source)
+         directory.mkdir(parents=True, exist_ok=True)
 
-typer.run(main)
+     def build(self):
+         self.dest.mkdir(parents=True, exist_ok=True)
+         for path in self.source.rglob("*"):
+             if path.is_dir():
+                 self.create_dir(path)
